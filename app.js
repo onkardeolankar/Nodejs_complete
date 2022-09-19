31e0554f6ea7729f9bbd7692046a35813c3934e3
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-09-19 23:42:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const server = http.createServer((req,res) =>{
+    console.log(req.url, req.headers,req.method);
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>My First Page</h1</body>');
+    res.write('</html>');
+    res.end();
+
+});
+
+server.listen(3000);
